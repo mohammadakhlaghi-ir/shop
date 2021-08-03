a05f9d454306c79648941edb0109888db80b3957
wolfsmad <michaelsevii17@gmail.com>
2021-08-04 01:05:20 +0430

    update some files

--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -30,6 +30,7 @@ namespace Shop
         //This method gets called by the runtime.Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddRazorPages();
             #region DataBase Context
 
